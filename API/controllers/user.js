282677fd2c1a3329428e0654053edc24ec6c3910
Hugo Langlais <74960083+Bedjes-Studio@users.noreply.github.com>
2023-04-26 14:12:19 -0400

    Add reset token api

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -4,6 +4,7 @@ const uuidv1 = require("uuid").v1;
 const config = require("../config");
 
 const { errorHandler } = require("./utils");
+const { sendMail } = require("./mail");
 const User = require("../models/user");
 const Token = require("../models/token");
 
@@ -12,8 +13,10 @@ exports.signup = (req, res, next) => {
         .hash(req.body.password, 10)
         .then((hash) => {
             const user = new User({
+                email: req.body.email,
                 username: req.body.username,
                 password: hash,
+                creditCard: null,
             });
 
             user.save()
@@ -45,7 +48,6 @@ exports.login = (req, res, next) => {
                     }
                     token = jwt.sign({ username: req.body.username }, config.server.key, { expiresIn: "24h" });
                     res.cookie("AUTH_COOKIE", token);
-                    console.log;
                     res.status(200).json({ token: token });
                 });
             })
@@ -57,30 +59,46 @@ exports.login = (req, res, next) => {
     }
 };
 
+exports.logout = (req, res, next) => {
+    res.clearCookie("AUTH_COOKIE");
+    res.status(200).json({ message: "Auth token clear" });
+};
+
 exports.generateResetToken = (req, res, next) => {
-    if (req.body.username) {
-        User.findOne({ username: req.body.username })
+    if (req.body.email) {
+        User.findOne({ email: req.body.email })
             .then((user) => {
                 if (!user) {
                     return res.status(401).json({ error: "Utilisateur non trouvé !" });
                 }
 
                 const token = new Token({
-                    key: username + "." + uuidv1(),
-                    username: req.body.username,
+                    key: uuidv1(),
+                    username: user.username,
                 });
 
                 sendMail(
-                    user.mail,
+                    user.email,
                     "[Pixa] Demande de réinitialisation de votre mot de passe",
-                    `Bonjour, 
+                    `Bonjour ` +
+                        user.username +
+                        `, 
                     
                     Une demande de réinitialisation de mot de passe vient d'être effectuée sur votre compte Pixa.
                     Veuillez suivre le lien suivant pour réinitialiser votre mot de passe :
                     
-                    http://localhost:8080/resetPassword/` + token.key
+                    http://localhost:8080/resetPassword/` +
+                        token.key
                 );
-                res.status(200).json({ message: "Mail de réinitialisation de mot de passe envoyé !" });
+
+                token
+                    .save()
+                    .then(() => {
+                        res.status(200).json({ message: "Mail de réinitialisation de mot de passe envoyé !" });
+                    })
+                    .catch((error) => {
+                        errorHandler(error, res);
+                    });
             })
             .catch((error) => {
                 errorHandler(error, res);
@@ -90,21 +108,32 @@ exports.generateResetToken = (req, res, next) => {
     }
 };
 
-exports.resetToken = (req, res, next) => {
-    if (req.body.username && req.body.password) {
+exports.resetPassword = (req, res, next) => {
+    if (req.body.username && req.body.password && req.body.token) {
         User.findOne({ username: req.body.username })
             .then((user) => {
                 if (!user) {
                     return res.status(401).json({ error: "Utilisateur non trouvé !" });
                 }
-                bcrypt.compare(req.body.password, user.password).then((valid) => {
-                    if (!valid) {
-                        return res.status(401).json({ error: "Mot de passe incorrect !" });
+
+                Token.findOne({ key: req.body.token }).then((token) => {
+                    if (!token) {
+                        return res.status(401).json({ error: "Token non trouvé !" });
                     }
-                    token = jwt.sign({ username: req.body.username }, config.server.key, { expiresIn: "24h" });
-                    res.cookie("AUTH_COOKIE", token);
-                    console.log;
-                    res.status(200).json({ token: token });
+                    if (token.username == user.username) {
+                        bcrypt.hash(req.body.password, 10).then((hash) => {
+                            Token.deleteOne({ key: req.body.token }).then(() => {
+                                User.updateOne({ username: req.body.username }, { password: hash }).then((result) => {
+                                    if (result.modifiedCount > 0) {
+                                        return res.status(200).json({ message: "Password updated" });
+                                    }
+                                    return res.status(500).json({ message: "Unable to update password" });
+                                });
+                            });
+                        });
+                    } else {
+                        res.status(401).json({ error: "Le token ne correspond pas à l'utilisateur" });
+                    }
                 });
             })
             .catch((error) => {
@@ -113,4 +142,18 @@ exports.resetToken = (req, res, next) => {
     } else {
         res.status(401).json({ error: "Merci de remplir tous les champs" });
     }
+};
+
+exports.updateCreditCard = (req, res, next) => {
+    User.updateOne({ username: req.auth.username }, { creditCard: req.body.creditCard })
+        .then((result) => {
+            console.log(result);
+            if (result.modifiedCount > 0) {
+                return res.status(200).json({ message: "Creadit card updated" });
+            }
+            return res.status(500).json({ message: "Unable to update credit card" });
+        })
+        .catch((error) => {
+            errorHandler(error, res);
+        });
 };
--- API/routes/user.js
+++ API/routes/user.js
@@ -5,5 +5,9 @@ const userCtrl = require("../controllers/user");
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
+router.get("/logout", userCtrl.logout);
+router.post("/updateCreditCard", userCtrl.updateCreditCard);
+router.post("/generateResetToken", userCtrl.generateResetToken);
+router.post("/resetPassword", userCtrl.resetPassword);
 
 module.exports = router;
